8ef3eb724549771902fba5b208b0ca021e1621b6
Nurullah ÖZTURK <nozturk@pirireis.edu.tr>
2024-11-06 16:20:13 +0300

    6.11.2024

---
+++ alistirma.py
@@ -0,0 +1,11 @@
+"""Comparing integers using if statements and comparison operatos."""
+
+print("İki sayı gir, aralarındaki ilişkiyi söyleyeyim.")
+
+# kullanıcıdan iki sayı al. değişkenlerde tut
+# eğer sayı1 sayı2 ile eşit ise sayı1, sayı2 ile eşittir yazdır
+# eğer sayı1 sayı2 ile eşit değilse sayı1, sayı2 ile eşit değildir yazdır
+# eğer sayı1 sayı2 den küçük ise sayı1, sayı2 den küçüktür yazdır
+# >, <=, >= için de aynı şekilde yapın...
+
+# İpucu: kullancıdan input() fonksiyonu ile girdi alabilirsiniz

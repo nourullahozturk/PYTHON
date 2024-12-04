fc91c0dbbc035134fda90608b114d9ce3c3b3e59
Nurullah ÖZTURK <nozturk@pirireis.edu.tr>
2024-12-04 15:49:38 +0300

    04.12.2024

--- 8-a10.py
+++ 8-a10.py
@@ -13,3 +13,25 @@
 # Program Ctrl + C ile durdurulana kadar bu şekilde yazdırmaya devam etsin.
 # hard kodlama yapmayın. Başa eklenen max boşluk sayısı ve satırdaki
 # yıldız miktarı kolayca değiştirilebilir olmalı. 
+
+import time
+MAX_INDENT = 20
+LINE_LENGTH = 8
+indent = 0
+indentIncreasing = True
+try:
+    while True:
+        print(" " * indent, end="")
+        print("*" * LINE_LENGTH)
+        time.sleep(0.5)
+        if indentIncreasing:
+            indent = indent + 1
+            if indent == MAX_INDENT:
+                indentIncreasing = False
+        else:
+            indent = indent - 1
+            if indent == 0:
+                indentIncreasing = True
+except KeyboardInterrupt:
+    print("Thanks for playing!")
+
--- 8-a9.py
+++ 8-a9.py
@@ -9,3 +9,19 @@
 # aynıysa
 # Congratulations! You guessed the number.
 # hata kontrollerini yapmayı unutmayın.
+
+import random, sys
+number_to_guess = random.randint(1, 100)
+while True:
+    try:
+        guess = int(input("Guess the number between 1 and 100: "))
+        if guess < number_to_guess:
+            print("Too low!")
+        elif guess > number_to_guess:
+            print("Too high!")
+        else:
+            print("Congratulations! You guess the number.")
+            # sys.exit()
+            break
+    except ValueError:
+        print("Please enter a valid number")
